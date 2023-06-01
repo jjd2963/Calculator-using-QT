dc22e979012b84f03f896b50a6caa85672654b86
jjd2963 <59725142+jjd2963@users.noreply.github.com>
2023-05-31 19:09:04 -0500

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,207 @@
+#include "calc.h"
+#include "qregularexpression.h"
+#include "ui_calc.h"
+#include <QRegularExpression>
+
+// Will define if this was the last math button clicked
+bool divTrigger = false;
+bool multTrigger = false;
+bool addTrigger = false;
+bool subTrigger = false;
+
+// Constructor
+calc::calc(QWidget *parent) :
+
+    // Call the QMainWindow constructor
+    QMainWindow(parent),
+
+    // Create the UI class and assign it to the ui member
+    ui(new Ui::calc)
+{
+
+    // Setup the UI
+    ui->setupUi(this);
+
+    calcVal = 0.0;
+    memVal = 0.0;
+    solution = 0.0;
+
+    // Put 0.0 in Display
+    ui->Display->setText(QString::number(calcVal));
+
+    // Will hold references to all the number buttons
+    QPushButton *numButtons[10];
+
+    // Cycle through locating the buttons
+    for(int i = 0; i < 10; ++i){
+        QString butName = "Button" + QString::number(i);
+
+        // Get the buttons by name and add to array
+        numButtons[i] = calc::findChild<QPushButton *>(butName);
+
+        // When the button is released call num_pressed()
+        connect(numButtons[i], SIGNAL(released()), this,
+                SLOT(NumPressed()));
+    }
+
+    // Connect signals and slots for math buttons
+    connect(ui->Add, SIGNAL(released()), this,
+            SLOT(MathButtonPressed()));
+    connect(ui->Subtract, SIGNAL(released()), this,
+            SLOT(MathButtonPressed()));
+    connect(ui->Multiply, SIGNAL(released()), this,
+            SLOT(MathButtonPressed()));
+    connect(ui->Divide, SIGNAL(released()), this,
+            SLOT(MathButtonPressed()));
+
+    // Connect equals button
+    connect(ui->Equals, SIGNAL(released()), this,
+            SLOT(EqualButtonPressed()));
+
+    // Connect change sign
+    connect(ui->ChangeSign, SIGNAL(released()), this,
+            SLOT(ChangeNumberSign()));
+
+    connect(ui->Clear,SIGNAL(released()),this,SLOT(Clear()));
+
+    // Connect the signals to the slot
+    connect(ui->MemAdd, SIGNAL(released()), this,
+            SLOT(MemAddClearGet()));
+    connect(ui->MemClear, SIGNAL(released()), this,
+            SLOT(MemAddClearGet()));
+    connect(ui->MemGet, SIGNAL(released()), this,
+            SLOT(MemAddClearGet()));
+
+}
+
+calc::~calc()
+{
+    delete ui;
+}
+
+void calc::NumPressed(){
+
+    // Sender returns a pointer to the button pressed
+    QPushButton *button = (QPushButton *)sender();
+
+    // Get number on button
+    QString butVal = button->text();
+
+    // Get the value in the display
+    QString displayVal = ui->Display->text();
+
+    if((displayVal.toDouble() == 0) || (displayVal.toDouble() == 0.0)){
+
+        // calcVal = butVal.toDouble();
+        ui->Display->setText(butVal);
+
+    } else {
+        // Put the new number to the right of whats there
+        QString newVal = displayVal + butVal;
+
+        // Double version of number
+        double dblNewVal = newVal.toDouble();
+
+        // calcVal = newVal.toDouble();
+
+        // Set value in display and allow up to 16
+        // digits before using exponents
+        ui->Display->setText(QString::number(dblNewVal, 'g', 16));
+
+    }
+}
+
+void calc::MathButtonPressed(){
+
+    // Cancel out previous math button clicks
+    divTrigger = false;
+    multTrigger = false;
+    addTrigger = false;
+    subTrigger = false;
+
+    // Store current value in Display
+    QString displayVal = ui->Display->text();
+    calcVal = displayVal.toDouble();
+
+    // Sender returns a pointer to the button pressed
+    QPushButton *button = (QPushButton *)sender();
+
+    // Get math symbol on the button
+    QString butVal = button->text();
+
+    if(QString::compare(butVal, "/", Qt::CaseInsensitive) == 0){
+        divTrigger = true;
+    } else if(QString::compare(butVal, "*", Qt::CaseInsensitive) == 0){
+        multTrigger = true;
+    } else if(QString::compare(butVal, "+", Qt::CaseInsensitive) == 0){
+        addTrigger = true;
+    } else {
+        subTrigger = true;
+    }
+
+    // Clear display
+    ui->Display->setText("");
+
+}
+
+void calc::EqualButtonPressed(){
+    // Get value in display
+    QString displayVal = ui->Display->text();
+    double dblDisplayVal = displayVal.toDouble();
+
+    // Make sure a math button was pressed
+    if(addTrigger || subTrigger || multTrigger || divTrigger ){
+        if(addTrigger){
+            solution = calcVal + dblDisplayVal;
+        } else if(subTrigger){
+            solution = calcVal - dblDisplayVal;
+        } else if(multTrigger){
+            solution = calcVal * dblDisplayVal;
+        } else {
+            solution = calcVal / dblDisplayVal;
+        }
+    }
+
+    // Put solution in display
+    ui->Display->setText(QString::number(solution));
+
+}
+void calc::ChangeNumberSign() {
+
+    QString displayVal = ui->Display->text();
+    QRegularExpression reg("[-]?[0-9.]*");
+    QRegularExpressionMatch match = reg.match(displayVal);
+    if(match.hasMatch()) {
+        double dblDisplayVal = displayVal.toDouble();
+        double dblDisplayValSign = -1 * dblDisplayVal;
+        ui->Display->setText(QString::number(dblDisplayValSign));
+    }
+
+}
+
+void calc::MemAddClearGet()
+{
+    QPushButton* button = qobject_cast<QPushButton*>(sender());
+    if (button)
+    {
+        QString buttonText = button->text();
+
+        if (buttonText == "M-")
+        {
+            memVal = 0.0;
+        }
+        else if (buttonText == "M+")
+        {
+            memVal += solution;
+        }
+        else
+        {
+            ui->Display->setText(QString::number(memVal, 'g', 16));
+        }
+    }
+}
+
+void calc::Clear() {
+    ui->Display->setText("0.0");
+}
+
